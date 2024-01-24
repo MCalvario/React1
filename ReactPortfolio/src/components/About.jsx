539b0d8235942e24a7c7ff423c0f3542be1e17fa
MaribelC <Maribel_Calvario@yahoo.com>
2024-01-24 00:29:33 -0600

    added pic

--- ReactPortfolio/src/components/About.jsx
+++ ReactPortfolio/src/components/About.jsx
@@ -1,18 +1,17 @@
 
 import '../styles/About.css';
 
-
 function About() {
-  // TODO: Add a style attribute to footer and the h1 element
-
 
   return (
-
     <about style={{ background: "black", color: "silver", justifyContent: "flex-end"}}
     className="about">
 
       <center>
       <h1>About Me</h1>
+
+      <img className="image" src='/20230709_071707.jpg' height="250" width="200" />
+
       <p className="about">My name is Maribel Calvario and I love coding! <br></br>
        I am passionate about web development and am looking forward to learning a lot more as I explore and research.<br></br>
        In my free time, I love spending time with my family and of course, I love coding.<br></br>
